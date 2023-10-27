3d17203f380d3662d1caee4b47153f9f4690bdd8
Kevin Kiambe <kevinkiambe@gmail.com>
2023-10-27 12:59:28 +0300

    Changed database seeting to postgres

--- egroup_backend/settings.py
+++ egroup_backend/settings.py
@@ -80,15 +80,15 @@ WSGI_APPLICATION = 'egroup_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+}"""
 
 
-"""DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'lifton2',
@@ -97,7 +97,7 @@ DATABASES = {
         'HOST': 'localhost',
         'PORT': '5432',
     }
-}"""
+}
 
 
 # Password validation
