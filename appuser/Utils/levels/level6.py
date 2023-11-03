89ebd58c6b48866c04536cb128e6d9cd8702e32d
daniel <albertagoya@gmail.com>
2023-11-03 13:03:16 +0300

    fixing

--- appuser/Utils/levels/level6.py
+++ appuser/Utils/levels/level6.py
@@ -1,9 +1,9 @@
 from appuser.Utils.menus import *
-def level6(custom_text,member_id):
+def level6(custom_text,member_id,phone_number):
     if custom_text[-2] == '1':
         # post to db
         member_save_to_group(custom_text,member_id)
-        return response_menu_want_to_save_yes_amount(custom_text,member_id)
+        return response_menu_want_to_save_yes_amount(custom_text,member_id,phone_number)
     elif custom_text[-2] == '2':
         return response_menu_pay_welfare_savings_stk(custom_text)
     elif custom_text[-2] == '3':
