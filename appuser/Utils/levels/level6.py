a0e054fd3eb87a5984bae8da2ae51ee8215ead79
albertdaniell <albertagoya@gmail.com>
2023-11-10 10:55:29 +0300

    fxing

--- appuser/Utils/levels/level6.py
+++ appuser/Utils/levels/level6.py
@@ -8,13 +8,17 @@ def level6(custom_text,member_id,phone_number):
         # check to see of amount is numeric or not
         amount = custom_text[-1]
         if amount.isnumeric():
-
             member_save_to_group(custom_text,member_id)
             return response_menu_want_to_save_yes_amount(custom_text,member_id,phone_number)
         else:
             return "END Amount is not numeric. Please try again."
     elif custom_text[-2] == '2':
-        return response_menu_pay_welfare_savings_stk(custom_text)
+        amount = custom_text_two[-1]
+        if amount.isnumeric():
+            return response_menu_pay_welfare_savings_stk(custom_text)
+        else:
+            return "END Amount is not numeric. Please try again."
+            
     elif custom_text[-2] == '3':
         return response_menu_pay_penalties_stk(custom_text)
 
